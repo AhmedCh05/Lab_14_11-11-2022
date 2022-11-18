ff3df903570d75d4f7a3722d8a2d950034eab34e
Ahmed Naeem <ahmed.chaudhry1520@gail.com>
2022-11-18 14:31:07 +0500

    Task_6

--- 6.js
+++ 6.js
@@ -1,12 +1,10 @@
 const collection = document.getElementsByTagName("img");
 for (let i = 0; i < collection.length; i++) {
-    if(i%2==0)
-    {
-        color="red";
+    if (i % 2 == 0) {
+        color = "red";
     }
-    else
-    {
-        color="green";
+    else {
+        color = "green";
     }
-  collection[i].style.border=" 5px solid "+color;
+    collection[i].style.border = " 5px solid " + color;
 }
